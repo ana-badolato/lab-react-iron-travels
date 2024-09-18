bbdf22dcf3ee75afd05f0c8987e9cdb3662b7801
Ana Badolato <badolatoana@gmail.com>
2024-09-18 21:19:08 +0200

    Iteration 3

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react"
 import travelPlansData from "../assets/travel-plans.json";
 
 function TravelList() {
+
+  const [travelArr, setTravel] = useState(travelPlansData);
+
+  const handleDelete = (index) => {
+    const clone = travelArr.slice(0);  // Crea una copia de travelArr
+    clone.splice(index, 1);            // Elimina un elemento en el índice especificado
+    setTravel(clone);                  // Actualiza el estado con el nuevo array
+  }
+
+
   return (
     <div>
-      
-      {travelPlansData.map((eachElement, index) => (
+      {travelArr.map((eachElement, index) => (
         <div className="page" key={index}>
           <div  className="container">
             <div className="img">
@@ -19,8 +29,8 @@ function TravelList() {
               {eachElement.totalCost >= 1500 ? <label>Premium</label> : null }
               {eachElement.totalCost <= 350 ? <label className="deal">Great Deal</label> : null }
               {eachElement.totalCost >= 1500? <label>All-inclusive</label> : null }
-              </div>
-      
+            </div>
+            <button onClick={() => handleDelete(index)}>Borrar</button>
             </div>
           </div>
         </div>
